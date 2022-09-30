d5086aeb0ed432c6a7cb9df177fbf958a3ce38d2
dephizee <hafiz227@gmail.com>
2022-09-30 18:12:04 +0100

    tokn contract

---
+++ contracts/IcmToken.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./common/IERC20.sol";
+import "./common/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20 is IERC20, Ownable {
+    uint256 public totalSupply = 1000000000;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "ICM Tokn";
+    string public symbol = "ICMT";
+    uint8 public decimals = 18;
+
+    constructor() Ownable() {
+        mint(msg.sender, totalSupply);
+    }
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        require(
+            allowance[sender][msg.sender] >= amount,
+            "Insufficient Allowance"
+        );
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(address _address, uint256 amount) internal {
+        balanceOf[_address] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), _address, amount);
+    }
+
+    function ownerMint(address _address, uint256 amount) public onlyOwner {
+        mint(_address, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
---
+++ contracts/Stake.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./common/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Stake is Ownable {
+    mapping(address => uint256) public stakeBalance;
+    mapping(address => address) public nodeAddresses;
+    mapping(address => address) public stakeAddresses;
+    bool public withdrawalEnabled;
+    bool public locked;
+    uint256 public minStake;
+    IERC20 tokenContract;
+    event StakeEvent(
+        address indexed account,
+        uint256 amount,
+        uint256 timestamp
+    );
+    event UnStakeEvent(
+        address indexed account,
+        uint256 amount,
+        uint256 timestamp
+    );
+
+    modifier noReentrancy() {
+        require(!locked, "Contract Locked");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    constructor(address _address) Ownable() {
+        tokenContract = IERC20(_address);
+    }
+
+    function stake(uint256 amount) public {
+        require(amount > 0, "You need to Stake at least some tokens");
+        // uint256 allowance = tokenContract.allowance(msg.sender, address(this));
+        // require(allowance >= amount, "Check the token allowance");
+        tokenContract.transferFrom(msg.sender, address(this), amount);
+        stakeBalance[msg.sender] += amount;
+        emit StakeEvent(msg.sender, amount, block.timestamp);
+    }
+
+    function enableWithdrawal(bool _enabled) public onlyOwner {
+        withdrawalEnabled = _enabled;
+    }
+
+    function unStake() public noReentrancy {
+        require(withdrawalEnabled, "Withdrawal is not enabled");
+        tokenContract.transfer(msg.sender, stakeBalance[msg.sender]);
+        emit UnStakeEvent(
+            msg.sender,
+            stakeBalance[msg.sender],
+            block.timestamp
+        );
+        stakeBalance[msg.sender] = 0;
+    }
+
+    function registerNodeAccount(address nodeAddress) public {
+        require(
+            msg.sender != nodeAddress,
+            "Node address can not be equal to stake address"
+        );
+        nodeAddresses[msg.sender] = nodeAddress;
+        stakeAddresses[nodeAddress] = msg.sender;
+    }
+
+    function setMinStake(uint256 _stake) public onlyOwner {
+        minStake = _stake;
+    }
+
+    function getNodeLevel(address _nodeAddresses)
+        public
+        view
+        returns (uint256)
+    {
+        address _staker = stakeAddresses[_nodeAddresses];
+        if (_staker == address(0)) {
+            return 0;
+        }
+        uint256 balance = stakeBalance[_staker];
+        if (balance < minStake) return 0;
+        return 1;
+    }
+}
