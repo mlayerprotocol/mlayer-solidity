54ca602157987bd96af72c9890291e90cc42c453
dephizee <hafiz227@gmail.com>
2022-10-31 19:07:57 +0100

    upgrade

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -34,11 +34,26 @@ contract Stake is OwnableUpgradeable {
     mapping(address => mapping(address => uint256)) public userMessages;
     mapping(bytes32 => Order ) public orders;
 
-    uint256 public stakerIndex;
+    uint256 public stakerCount;
     mapping(uint256 => address) public stakerIds;
     mapping(address => uint256) public stakerIdsRef;
 
-    uint256 public minConst;
+    uint256 public minStakable;
+    uint256 public calibrator;
+
+    struct AllocationStruct{
+        uint256 price;
+        uint256 count;
+    }
+
+    mapping(address => AllocationStruct[]) public nodeAllocation;
+
+    mapping(address => uint256) public nodeAllocationIndexes;
+    mapping(address => address[]) public stakerNodeAddresses;
+
+    uint256 public nodeCount;
+    mapping(uint256 => address) public nodeIds;
+    mapping(address => uint256) public nodeIdsRef;
 
     event StakeEvent(
         address indexed account,
@@ -61,6 +76,8 @@ contract Stake is OwnableUpgradeable {
     
     function initialize(address _address) public initializer {
         tokenContract = IERC20(_address);
+        minStakable = 5000 * 10**18;
+        calibrator= 10000;
     }
 
     function stake(uint256 amount) public {
@@ -68,14 +85,30 @@ contract Stake is OwnableUpgradeable {
         require(amount >= getMinStake(), "You need to Stake at least some tokens");
 
         if(stakerIdsRef[msg.sender] == 0 ){
-            stakerIndex++;
-            stakerIds[stakerIndex] == msg.sender;
-            stakerIdsRef[msg.sender] == stakerIndex;
+            stakerCount++;
+            stakerIds[stakerCount] == msg.sender;
+            stakerIdsRef[msg.sender] == stakerCount;
         }
         // uint256 allowance = tokenContract.allowance(msg.sender, address(this));
         // require(allowance >= amount, "Check the token allowance");
         tokenContract.transferFrom(msg.sender, address(this), amount);
         stakeBalance[msg.sender] += amount;
+        int existingAlloctionIndex = -1;
+        for (uint i = 0; i < nodeAllocation[msg.sender].length; i++) {
+            if(nodeAllocation[msg.sender][i].price == getMinStake()){
+                existingAlloctionIndex = int(i);
+                break;
+            }
+        }
+        if(existingAlloctionIndex == -1){
+            nodeAllocation[msg.sender].push(AllocationStruct({
+                price:  getMinStake(),
+                count:  amount/getMinStake()
+            }));
+        }else{
+            nodeAllocation[msg.sender][uint(existingAlloctionIndex)].count += amount/getMinStake();
+        }
+        
         emit StakeEvent(msg.sender, amount, block.timestamp);
     }
 
@@ -85,6 +118,15 @@ contract Stake is OwnableUpgradeable {
 
     function unStake() public noReentrancy {
         require(withdrawalEnabled, "Withdrawal is not enabled");
+
+        uint total;
+        for (uint i = 0; i < nodeAllocation[msg.sender].length; i++) {
+            if(nodeAllocation[msg.sender][i].count > 0){
+                total += nodeAllocation[msg.sender][i].count * nodeAllocation[msg.sender][i].price;
+                nodeAllocation[msg.sender][i].count = 0;
+            }
+        }
+        require(total > 0, "Inadequate Withdrawal Balance");
         tokenContract.transfer(msg.sender, stakeBalance[msg.sender]);
         emit UnStakeEvent(
             msg.sender,
@@ -92,15 +134,77 @@ contract Stake is OwnableUpgradeable {
             block.timestamp
         );
         stakeBalance[msg.sender] = 0;
+        
     }
 
-    function registerNodeAccount(address nodeAddress) public {
+    function withdrawableAmount() public view returns (uint) {
+
+        uint total;
+        for (uint i = 0; i < nodeAllocation[msg.sender].length; i++) {
+            if(nodeAllocation[msg.sender][i].count > 0){
+                total += nodeAllocation[msg.sender][i].count * nodeAllocation[msg.sender][i].price;
+            }
+        }
+        return total;
+        
+    }
+
+    function registerNodeAccount(address nodeAddress) public noReentrancy {
         require(
             msg.sender != nodeAddress,
             "Node address can not be equal to stake address"
         );
-        nodeAddresses[msg.sender] = nodeAddress;
-        stakeAddresses[nodeAddress] = msg.sender;
+        require(
+            stakeAddresses[nodeAddress] == address(0),
+            "Node already exist"
+        );
+
+        bool success;
+        for (uint i = 0; i < nodeAllocation[msg.sender].length; i++) {
+            if(nodeAllocation[msg.sender][i].count > 0){
+                nodeAllocation[msg.sender][i].count -= 1;
+                // nodeAddresses[msg.sender] = nodeAddress;
+                stakeAddresses[nodeAddress] = msg.sender;
+                nodeAllocationIndexes[nodeAddress] = i;
+                stakerNodeAddresses[msg.sender].push(
+                    nodeAddress
+                );
+                success = true;
+
+                if(nodeIdsRef[nodeAddress] == 0 ){
+                    nodeCount++;
+                    nodeIds[nodeCount] == nodeAddress;
+                    nodeIdsRef[nodeAddress] == nodeCount;
+                }
+                break;
+            }
+        }
+        require(
+            success,
+            "Inadequate Stake Amount"
+        );
+    }
+
+    function deRegisterNodeAccount(address nodeAddress) public noReentrancy {
+        require(
+            msg.sender != nodeAddress,
+            "Node address can not be equal to stake address"
+        );
+        require(
+            stakeAddresses[nodeAddress] == msg.sender,
+            "Not Authorized"
+        );
+        uint i = nodeAllocationIndexes[nodeAddress];
+        nodeAllocation[msg.sender][i].count += 1;
+        // nodeAddresses[msg.sender] = address(0);
+        stakeAddresses[nodeAddress] = address(0);
+        for (uint j = 0; j < stakerNodeAddresses[msg.sender].length; j++) {
+            if(stakerNodeAddresses[msg.sender][j] == nodeAddress){
+                stakerNodeAddresses[msg.sender][j] = address(0);
+            }
+            
+        }
+        
     }
 
     function setMinStake(uint256 _stake) public onlyOwner {
@@ -113,11 +217,9 @@ contract Stake is OwnableUpgradeable {
         returns (uint256)
     {
         address _staker = stakeAddresses[_nodeAddresses];
-        if (_staker == address(0)) {
+        if (_staker == address(0) || stakeBalance[_staker] == 0) {
             return 0;
         }
-        uint256 balance = stakeBalance[_staker];
-        if (balance < getMinStake()) return 0;
         return 1;
     }
 
@@ -152,10 +254,15 @@ contract Stake is OwnableUpgradeable {
         view
         returns (uint256)
     {
-        return minConst + (minConst * stakerIndex**2);   
+        // return minConst * (1 + (stakerCount/100)**2);   
+        return minStakable + ((minStakable*(stakerCount**2))/calibrator);   
     }
 
-    function setMinConst(uint256 _const) public onlyOwner {
-        minConst = _const;
+    function setMinStakable(uint256 _minStakable) public onlyOwner {
+        minStakable = _minStakable;
+    }
+
+    function setCalibrator(uint256 _calibrator) public onlyOwner {
+        calibrator = _calibrator;
     }
 }
